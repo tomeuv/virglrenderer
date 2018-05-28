21dbbb3e7754a008ccc8e1130696311e9c5dd160
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-05-28 10:18:06 +0200

    don't block

--- run-deqp.sh
+++ run-deqp.sh
@@ -1,5 +1,5 @@
 systemctl start weston
 sleep 3
-systemctl status weston
+PAGER= systemctl status weston
 WAYLAND_DISPLAY=wayland-0 $ROOT_DIR/install-prefix/VK-GL-CTS/build/external/openglcts/modules/glcts --deqp-case=dEQP-GLES3.*
 
