fd46dab0bad138ff9b4b61ea485c49cf4e2f80ad
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-05-28 10:35:38 +0200

    use gles qemu branch

--- run-deqp.sh
+++ run-deqp.sh
@@ -1,5 +1,7 @@
 systemctl start weston
 sleep 3
 PAGER= systemctl status weston
+PAGER= journalctl -b
 WAYLAND_DISPLAY=wayland-0 $ROOT_DIR/install-prefix/VK-GL-CTS/build/external/openglcts/modules/glcts --deqp-case=dEQP-GLES3.*
+sleep 3
 
