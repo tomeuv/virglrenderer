ac8f30e64ca92469edd8730b26cafe0c7ef6bfd6
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-07 14:42:18 +0200

    Fix path

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,4 +1,4 @@
 nohup weston --backend=headless-backend.so &
 sleep 2
-fakemachine -v /etc/pam.d:/etc/pam.d -v /virglrenderer:/virglrenderer sh /virglrenderer/run-deqp.sh
+fakemachine -v /etc/pam.d:/etc/pam.d -v /virglrenderer:/virglrenderer sh /virglrenderer/ci/run-deqp.sh
 
