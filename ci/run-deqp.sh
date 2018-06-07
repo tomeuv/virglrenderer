ce42c72093b36e60974eec01280b2cd199a8ab76
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-07 15:51:47 +0200

    log weston

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -1,8 +1,10 @@
 export XDG_RUNTIME_DIR=/tmp
 
 echo "Starting weston in the guest"
-/usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --use-pixman
+nohup /usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --use-pixman --log weston.log &
 sleep 3
+
+cat weston.log
 
 echo "Running dEQP-GLES3.*"
 WAYLAND_DISPLAY=wayland-0 /usr/local/VK-GL-CTS/build/external/openglcts/modules/glcts --deqp-case=dEQP-GLES3.*
