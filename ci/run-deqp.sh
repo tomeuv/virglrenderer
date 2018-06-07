07a33e075b63b3046c71e6d648981de74f7286f8
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-07 15:00:48 +0200

    Fix path

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -1,10 +1,8 @@
 echo "Starting weston in the guest"
-systemctl start weston
+/usr/bin/weston --use-pixman
 sleep 3
-PAGER= systemctl status weston
-PAGER= journalctl -b
 
 echo "Running dEQP-GLES3.*"
-WAYLAND_DISPLAY=wayland-0 $ROOT_DIR/install-prefix/VK-GL-CTS/build/external/openglcts/modules/glcts --deqp-case=dEQP-GLES3.*
+WAYLAND_DISPLAY=wayland-0 /usr/local/VK-GL-CTS/build/external/openglcts/modules/glcts --deqp-case=dEQP-GLES3.*
 sleep 3
 
