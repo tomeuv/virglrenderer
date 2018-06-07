17d7efb8fc154661584cc428d2943fcc9ef5b043
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-07 15:33:33 +0200

    install kbd

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,6 +2,8 @@ echo "Starting weston in the host"
 nohup weston --backend=headless-backend.so &
 sleep 3
 
+DEBIAN_FRONTEND=noninteractive apt-get -y install kbd
+
 echo "Starting guest"
 fakemachine -v /etc/pam.d:/etc/pam.d -v /virglrenderer:/virglrenderer sh /virglrenderer/ci/run-deqp.sh
 
