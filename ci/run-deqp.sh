64c0a319975aec8678382b355455e1250c015b00
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-08 07:46:57 +0200

    List passing tests in files

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -1,13 +1,17 @@
 export XDG_RUNTIME_DIR=/tmp
 
+# TODO This should be started as a systemd service
 echo "Starting weston in the guest"
 nohup /usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --use-pixman --log weston.log &
 sleep 3
 
-cat weston.log
-
-echo "Running dEQP-GLES3.*"
 cd /usr/local/VK-GL-CTS/build/external/openglcts/modules
-WAYLAND_DISPLAY=wayland-0 ./glcts --deqp-case=dEQP-GLES3.*
+
+echo "Running GLES2 on GLES"
+WAYLAND_DISPLAY=wayland-0 ./glcts --deqp-caselist-file=/virglrenderer/ci/deqp-gles2-gles.txt
+
+echo "Running GLES3 on GLES"
+WAYLAND_DISPLAY=wayland-0 ./glcts --deqp-caselist-file=/virglrenderer/ci/deqp-gles3-gles.txt
+
 sleep 3
 
