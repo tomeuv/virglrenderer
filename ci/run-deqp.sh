fcd23158dc03835bf40eb405e3a5e28f4aa0f2cc
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-08 11:10:44 +0200

    Set some env vars for piglit

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -8,5 +8,10 @@ sleep 3
 cd /usr/local/piglit
 mkdir -p /virglrenderer/results
 
-WAYLAND_DISPLAY=wayland-0 ./piglit -x glx tests/deqp_gles3.py tests/opengl.py /virglrenderer/results
+export WAYLAND_DISPLAY=wayland-0
+export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable --deqp-crashhandler=enable"
+export PIGLIT_DEQP_GLES2_BIN=/usr/local/VK-GL-CTS/build/modules/gles2/deqp-gles2
+export PIGLIT_DEQP_GLES3_BIN=/usr/local/VK-GL-CTS/build/modules/gles3/deqp-gles3
+
+./piglit run -x glx tests/deqp_gles2.py tests/deqp_gles3.py tests/opengl.py /virglrenderer/results
 
