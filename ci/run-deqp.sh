354864d679d921ef306a98bd1a749ff4ac89ae15
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-08 13:03:39 +0200

    Test speed

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -5,13 +5,33 @@ echo "Starting weston in the guest"
 nohup /usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --use-pixman --log weston.log &
 sleep 3
 
-cd /usr/local/piglit
-mkdir -p /virglrenderer/results
-
 export WAYLAND_DISPLAY=wayland-0
 export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable --deqp-crashhandler=enable"
 export PIGLIT_DEQP_GLES2_BIN=/usr/local/VK-GL-CTS/build/modules/gles2/deqp-gles2
 export PIGLIT_DEQP_GLES3_BIN=/usr/local/VK-GL-CTS/build/modules/gles3/deqp-gles3
 
-./piglit run -p wayland -t deqp-gles2/capability/limits deqp_gles2 deqp_gles3 /virglrenderer/results
+cd /usr/local/VK-GL-CTS/build/external/openglcts/modules/
+time ./glcts --deqp-case=dEQP-GLES2.capability.limits.*
 
+time ./glcts --deqp-case=dEQP-GLES2.capability.limits.* --deqp-watchdog=enable
+
+time ./glcts --deqp-case=dEQP-GLES2.capability.limits.* --deqp-crashhandler=enable
+
+time ./glcts --deqp-case=dEQP-GLES2.capability.limits.* --deqp-watchdog=enable --deqp-crashhandler=enable
+
+cd /usr/local/piglit
+mkdir -p /virglrenderer/results
+
+export PIGLIT_DEQP_EXTRA_ARGS=""
+time ./piglit run -p wayland -t limits deqp_gles2 /virglrenderer/results
+
+export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable"
+time ./piglit run -p wayland -t limits deqp_gles2 /virglrenderer/results
+
+export PIGLIT_DEQP_EXTRA_ARGS="--deqp-crashhandler=enable"
+time ./piglit run -p wayland -t limits deqp_gles2 /virglrenderer/results
+
+export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable --deqp-crashhandler=enable"
+time ./piglit run -p wayland -t limits deqp_gles2 /virglrenderer/results
+
+#./piglit run -p wayland deqp_gles2 deqp_gles3 /virglrenderer/results
--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,6 +2,8 @@ echo "Starting weston in the host"
 nohup weston --backend=headless-backend.so &
 sleep 3
 
+DEBIAN_FRONTEND=noninteractive apt-get -y install time
+
 echo "Starting guest"
 fakemachine -v /virglrenderer:/virglrenderer sh /virglrenderer/ci/run-deqp.sh
 
