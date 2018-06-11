d1edb0f5cb567f697be49c9141cd6cb8cac5c0be
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-11 12:08:26 +0200

    Remove tests requiring big OpenGL for now

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,6 +1,6 @@
 echo "Starting weston in the host"
 nohup weston --backend=headless-backend.so &
-sleep 3
+sleep 1
 
 echo "Starting guest"
 fakemachine -v /virglrenderer:/virglrenderer sh /virglrenderer/ci/run-deqp.sh
