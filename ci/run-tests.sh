13f7ef7bf642ff719eac90500972204c0a383de2
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-12 11:05:36 +0200

    perf

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -2,7 +2,7 @@ echo "Starting weston in the host"
 nohup weston --backend=headless-backend.so &
 sleep 1
 
-DEBIAN_FRONTEND=noninteractive apt-get -y install sysstat  linux-base  
+DEBIAN_FRONTEND=noninteractive apt-get -y install sysstat  linux-base   linux-perf-4.16 
 
 export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable --deqp-crashhandler=enable"
 export PIGLIT_DEQP_GLES2_BIN=/usr/local/VK-GL-CTS/modules/gles2/deqp-gles2
