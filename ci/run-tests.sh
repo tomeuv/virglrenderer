2b443c562a5bbc886313a7cb558f7c6fff56ca3d
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-12 10:01:49 +0200

    Concurrent 2

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -10,6 +10,8 @@ export PIGLIT_DEQP_GLES3_BIN=/usr/local/VK-GL-CTS/modules/gles3/deqp-gles3
 cd /usr/local/piglit
 mkdir -p /virglrenderer/results2
 
+time ./piglit run -c -t color_c -p wayland deqp_gles2 /virglrenderer/results2
+time ./piglit run -t color_c -p wayland deqp_gles2 /virglrenderer/results2
 
 iostat -mxzs 5 &
 time ./piglit run -t color_c -p wayland deqp_gles2 /virglrenderer/results2
