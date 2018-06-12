480e7cf3b13582e56c765a3c3e04c256badca75e
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-12 12:11:52 +0200

    perf

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -13,7 +13,7 @@ cd /usr/local/piglit
 mkdir -p /virglrenderer/results2
 
 #iostat -mxzs 5 &
-perf record ls
+perf_4.16 record ls
 
 time perf_4.16 record -- ./piglit run -j 12 -c -t color_c -p wayland deqp_gles2 /virglrenderer/results2
 PAGER= perf_4.16 report --stdio
