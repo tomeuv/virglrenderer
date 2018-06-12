c3ce3e83509d9375eb73fb423c234ed50364930e
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-12 13:16:09 +0200

    improve report

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -18,8 +18,8 @@ mkdir -p /virglrenderer/results
 
 
 #iostat -mxzs 5 &
-time perf record -ag ./piglit run -j12 -c -t color_clear_value_ -p wayland deqp_gles2 /virglrenderer/results
-PAGER= perf report -n -g folded -v --stdio | head -1000
+time perf record -a ./piglit run -j12 -c -t color_clear_value_ -p wayland deqp_gles2 /virglrenderer/results
+PAGER= perf report -v --stdio | head -1000
 #killall iostat
 
 
