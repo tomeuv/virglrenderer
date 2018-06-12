691ea94a49cd317ed047c3dc1b87e5c38014d085
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-12 14:59:52 +0200

    Remove --use-pixman so swrast isn't forced

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -5,7 +5,7 @@ export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/lib
 
 # TODO This should be started as a systemd service
 echo "Starting weston in the guest"
-nohup /usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --use-pixman --log weston.log &
+nohup /usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --log weston.log &
 sleep 1
 
 export WAYLAND_DISPLAY=wayland-0
@@ -15,7 +15,6 @@ export PIGLIT_DEQP_GLES3_BIN=/usr/local/VK-GL-CTS/modules/gles3/deqp-gles3
 
 cd /usr/local/piglit
 mkdir -p /virglrenderer/results
-
 
 ./piglit run -j12 -c -p wayland deqp_gles2 /virglrenderer/results
 
