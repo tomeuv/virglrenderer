a99b2e4d6592d8355bcb1cf068409306ffd2639d
Tomeu Vizoso <tomeu.vizoso@collabora.com>
2018-06-14 07:58:57 +0200

    Build SDL from my branch

--- ci/run-deqp.sh
+++ ci/run-deqp.sh
@@ -1,12 +1,13 @@
-set -x
+set -ex
 
 export XDG_RUNTIME_DIR=/tmp
 export LD_LIBRARY_PATH=/usr/local/lib64:/usr/local/lib
 
 # TODO This should be started as a systemd service
 echo "Starting weston in the guest"
-nohup /usr/bin/openvt -c 7 -w -v -s -- weston --no-input --backend=drm-backend.so --log weston.log &
+nohup /usr/bin/openvt -c 7 -w -v -s -- weston --backend=drm-backend.so --log weston.log &
 sleep 1
+cat weston.log
 
 export WAYLAND_DISPLAY=wayland-0
 export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable --deqp-crashhandler=enable"
@@ -16,14 +17,7 @@ export PIGLIT_DEQP_GLES3_BIN=/usr/local/VK-GL-CTS/modules/gles3/deqp-gles3
 cd /usr/local/piglit
 mkdir -p /virglrenderer/results
 
-./piglit run -j12 -c -p wayland deqp_gles2 /virglrenderer/results
-
-#iostat -mxzs 5 &
-#time perf record ./piglit run -j12 -c -t color_clear_value_ -p wayland deqp_gles2 /virglrenderer/results
-#PAGER= perf report -v --stdio | head -1000
-#killall iostat
-
-
+./piglit run -c -p wayland deqp_gles2 deqp_gles3 /virglrenderer/results
 ./piglit summary console /virglrenderer/results
 
 sleep 3
--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,28 +1,13 @@
-set -x
+set -ex
 
-mkdir -p /etc/xdg/weston && printf "[core]\nrequire-input=false\n" > /etc/xdg/weston/weston.ini
+#DEBIAN_FRONTEND=noninteractive apt-get -y install apitrace
 
 echo "Starting weston in the host"
+mkdir -p /etc/xdg/weston
+printf "[core]\nrequire-input=false\n" > /etc/xdg/weston/weston.ini
 nohup /usr/bin/openvt -c 8 -w -v -- weston --backend=drm-backend.so --log weston.log &
-sleep 3
+sleep 1
 cat weston.log
-ps ax | grep weston
-cat /etc/xdg/weston/weston.ini
-
-#DEBIAN_FRONTEND=noninteractive apt-get -y install sysstat  linux-base   linux-perf-4.16 
-
-export PIGLIT_DEQP_EXTRA_ARGS="--deqp-watchdog=enable --deqp-crashhandler=enable"
-export PIGLIT_DEQP_GLES2_BIN=/usr/local/VK-GL-CTS/modules/gles2/deqp-gles2
-export PIGLIT_DEQP_GLES3_BIN=/usr/local/VK-GL-CTS/modules/gles3/deqp-gles3
-cd /usr/local/piglit
-mkdir -p /virglrenderer/results2
-
-#iostat -mxzs 5 &
-
-#time perf_4.16 record -o perf.data -- ./piglit run -j 12 -c -t color_clear_value_ -p wayland deqp_gles2 /virglrenderer/results2
-#PAGER= perf_4.16 report -i ./perf.data -v --stdio | head -1000
-#killall iostat
-
 
 echo "Starting guest"
 fakemachine -v /virglrenderer:/virglrenderer sh /virglrenderer/ci/run-deqp.sh
